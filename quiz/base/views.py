f8af5ee03afad086b5adc372a863e5a6c8eacb8e
Paula Sena <senasilvaanapaula@gmail.com>
2021-03-27 18:39:39 -0300

    Dia2 - classificacao.html | views.py | urls.py.
    Copia da pag end.html para classificacao.html criada rota no urls.py e feito o request na views.py.

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render
 def home(requisicao):
     return render(requisicao,'base/home.html')
 
+def classificacao(requisicao):
+    return render(requisicao,'base/classificacao.html')
+
 def perguntas(requisicao, indice):
     contexto ={'indice_da_questao': indice}
     return render(requisicao,'base/game.html',context=contexto)
